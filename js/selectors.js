8c5bf6aa003dc2cb961e3f7c14d3a3afc3676b52
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-12 23:20:18 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector () {
+  return $('p');
+}
+
+function lastImageSelector () {
+  
+}
+
+function ninjaBabySelector () {
+  
+}
+
+function divSelector () {
+  
+}
+
+function firstListItem () {
+  
+}
